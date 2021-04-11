d43e7316bc7b112ca3e06c777fb68fc02289b9d5
Daniel YU <i@icymint.me>
2021-04-11 22:08:36 +0800

    add log in factory

--- salak_factory/examples/redis.rs
+++ salak_factory/examples/redis.rs
@@ -7,7 +7,7 @@ fn main() {
         .with_default_args(auto_read_sys_args_param!())
         .build();
     let _ = env.build::<TracingLogConfig>().unwrap();
-    for (k, o, v) in RedisConfig::list_keys("primary") {
+    for (k, o, v) in RedisConfig::list_keys(DEFAULT_NAMESPACE) {
         if let Some(v) = v {
             info!("{}[required={}]: {}", k, o, v);
         } else {
