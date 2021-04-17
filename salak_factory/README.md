92e4da98a18abcd1f979587da013a0c41aaf3908
Daniel YU <i@icymint.me>
2021-04-17 20:51:02 +0800

    update ReadMe

--- salak_factory/README.md
+++ salak_factory/README.md
@@ -1,2 +1,19 @@
 #salak_factory
 
+
+### example toy_log
+```bash
+cargo install --example toy_log --features='enable_log' --path .
+```
+
+### toy_logger vs env_logger
+```bash
+time toy_log -P count=10000000 > /dev/null           
+# Record 10000000 logs in 3905ms, 390ns/log, 10242994/s, 2560748/s/thread
+# toy_log -P count=10000000 > /dev/null  3.60s user 0.15s system 380% cpu 0.985 total
+
+
+time toy_log -P count=10000000 -P env_log=true > /dev/null      
+# Record 10000000 logs in 70219ms, 7021ns/log, 569642/s, 142410/s/thread
+# toy_log -P count=10000000 -P env_log=true > /dev/null  16.42s user 35.60s system 287% cpu 18.064 total
+```
--- salak_factory/examples/toy_log.rs
+++ salak_factory/examples/toy_log.rs
@@ -1,3 +1,4 @@
+use chrono::Utc;
 use log::*;
 use salak::*;
 use salak_factory::*;
@@ -19,26 +20,43 @@ fn main() {
     let env = Salak::new()
         .with_default_args(auto_read_sys_args_param!())
         .build();
-    let max = env.load_config::<Max>().unwrap();
-    if max.env_log {
-        let _ = env_logger::init();
+    let conf = env.load_config::<Max>().unwrap();
+    if conf.env_log {
+        let _ = env_logger::builder()
+            .filter_level(LevelFilter::Info)
+            .target(env_logger::Target::Stdout)
+            .init();
     } else {
         let _ = set_global_default(registry().with(env.build::<LogConfig>().unwrap()));
     }
 
-    let num = max.thread.unwrap_or(num_cpus::get_physical()).max(1);
-    let max = max.count / num;
+    let num = conf.thread.unwrap_or(num_cpus::get_physical()).max(1);
+    let total = conf.count;
+    let max = conf.count / num;
     let mut join = vec![];
     for i in 0..num {
         join.push(std::thread::spawn(move || {
+            let t = Utc::now();
             let i = i * max;
             for j in 0..max {
                 info!("Hello {:0>10}", i + j);
             }
+            Utc::now().timestamp_nanos() - t.timestamp_nanos()
         }));
     }
 
+    let mut time = 0;
     for h in join {
-        let _ = h.join();
+        if let Ok(t) = h.join() {
+            time += t;
+        }
     }
+    eprintln!(
+        "Record {} logs in {}ms, {}ns/log, {}/s, {}/s/thread",
+        total,
+        time / 1000_000,
+        time / (total as i64),
+        ((num * total) as i64) * 1000_000_000 / time,
+        (total as i64) * 1000_000_000 / time
+    );
 }
