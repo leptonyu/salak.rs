e297cb59bdbb588ce1d61772f5aa253fdf9171ef
Daniel YU <i@icymint.me>
2021-05-14 22:35:56 +0800

    refactor salak - fix test

--- test.sh
+++ test.sh
@@ -4,22 +4,19 @@ cargo test --verbose
 cargo test --verbose --all-features
 cargo test --verbose --lib ## Default feature
 cargo test --verbose --lib --no-default-features 
-cargo test --verbose --lib --no-default-features --features=enable_log
-cargo test --verbose --lib --no-default-features --features=enable_clap
-cargo test --verbose --lib --no-default-features --features=enable_pico
-cargo test --verbose --lib --no-default-features --features=enable_toml
-cargo test --verbose --lib --no-default-features --features=enable_yaml
-cargo test --verbose --lib --no-default-features --features=enable_derive
+cargo test --verbose --lib --no-default-features --features=toml
+cargo test --verbose --lib --no-default-features --features=yaml
+cargo test --verbose --lib --no-default-features --features=derive
 
-cargo run --example salak
+# cargo run --example salak
 
-cd salak_factory
-cargo test --verbose --all-features
-cargo test --verbose --lib ## Default feature
-cargo test --verbose --lib --no-default-features --features=enable_redis
-cargo test --verbose --lib --no-default-features --features=enable_redis_cluster
-cargo test --verbose --lib --no-default-features --features=enable_postgres
-cargo test --verbose --lib --no-default-features --features=enable_log
+# cd salak_factory
+# cargo test --verbose --all-features
+# cargo test --verbose --lib ## Default feature
+# cargo test --verbose --lib --no-default-features --features=enable_redis
+# cargo test --verbose --lib --no-default-features --features=enable_redis_cluster
+# cargo test --verbose --lib --no-default-features --features=enable_postgres
+# cargo test --verbose --lib --no-default-features --features=enable_log
 
-cargo run --example redis --features='default enable_log enable_redis'
-cd -
+# cargo run --example redis --features='default enable_log enable_redis'
+# cd -
