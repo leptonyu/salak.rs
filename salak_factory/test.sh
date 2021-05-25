e083054b58ea594cc493d65bcf87df4b22cd7731
Daniel YU <i@icymint.me>
2021-05-25 20:26:04 +0800

    update internal desc

---
+++ salak_factory/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+cargo test --verbose --all-features
+cargo test --verbose --lib ## Default feature
+cargo test --verbose --lib --no-default-features --features=redis_default
+cargo test --verbose --lib --no-default-features --features=redis_cluster
+cargo test --verbose --lib --no-default-features --features=postgresql
+
+cargo run --example redis --features='default redis_default salak/args'
--- test.sh
+++ test.sh
@@ -11,13 +11,3 @@ cargo test --verbose --lib --no-default-features --features=args
 
 cargo bench 
 cargo run --example salak --features='default args' -- -h
-
-cd salak_factory
-cargo test --verbose --all-features
-cargo test --verbose --lib ## Default feature
-cargo test --verbose --lib --no-default-features --features=redis_default
-cargo test --verbose --lib --no-default-features --features=redis_cluster
-cargo test --verbose --lib --no-default-features --features=postgresql
-
-cargo run --example redis --features='default redis_default salak/args'
-cd -
