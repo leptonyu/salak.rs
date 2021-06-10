8aaa8edbcdd950a9710146b033a56891d7ca6498
Daniel YU <i@icymint.me>
2021-06-10 20:05:36 +0800

    use btreemap

--- benches/app.rs
+++ benches/app.rs
@@ -11,6 +11,49 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("res1", |b| b.iter(|| env.init_resource::<()>()));
     c.bench_function("res2", |b| b.iter(|| env.get_resource::<()>()));
+    let mut builder = Salak::builder().set("hello", "world");
+
+    builder = builder
+        .register_default_resource::<()>()
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-0"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-1"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-2"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-3"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-4"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-5"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-6"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-7"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-8"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-10"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-11"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-12"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-13"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-14"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-15"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-16"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-17"))
+        .unwrap()
+        .register_resource::<()>(ResourceBuilder::new("res-18"))
+        .unwrap();
+    let env = builder.build().unwrap();
+    c.bench_function("res3", |b| b.iter(|| env.get_resource::<()>()));
 }
 
 criterion_group!(benches, criterion_benchmark);
