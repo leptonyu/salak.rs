890d298189ecf5bfa98d02672b6f4bda4fdfe8f7
Daniel YU <i@icymint.me>
2021-06-12 23:06:56 +0800

    update sysinfo metrics

--- salak_factory/src/metric.rs
+++ salak_factory/src/metric.rs
@@ -6,6 +6,7 @@ use parking_lot::Mutex;
 use salak::*;
 use std::{
     any::Any,
+    collections::HashSet,
     net::SocketAddr,
     ops::Deref,
     sync::Arc,
@@ -20,7 +21,9 @@ pub struct Metric {
     recorder: PrometheusRecorder,
     handle: PrometheusHandle,
     code: Mutex<Vec<Box<dyn Fn(&Metric) -> Result<(), PropertyError> + Send + 'static>>>,
-    sys: System,
+    sys: Mutex<System>,
+    enabled: bool,
+    networks: HashSet<String>,
 }
 
 impl Deref for Metric {
@@ -55,34 +58,49 @@ pub trait AnyKey: Any + Resource {
 impl<T: Any + Resource> AnyKey for T {}
 
 macro_rules! set_val {
-    ($labels:ident:$self:ident.$fn:ident => $val:expr) => {
-        if let Some(n) = $self.sys.$fn() {
+    ($labels:ident:$sys:ident.$fn:ident => $val:expr) => {
+        if let Some(n) = $sys.$fn() {
             $labels.push(Label::new($val, n))
         }
     };
 }
 
 macro_rules! gauge_kb {
-    ($metric:ident.$fn:ident = $name:expr) => {
-        $metric.gauge($name, ($metric.sys.$fn() * 1024) as f64);
+    ($metric:ident.$sys:ident.$fn:ident = $name:expr) => {
+        $metric.gauge($name, ($sys.$fn() * 1024) as f64);
     };
 }
 
 macro_rules! gauge {
-    ($metric:ident.$fn:ident = $name:expr) => {
-        $metric.gauge($name, $metric.sys.$fn() as f64);
+    ($metric:ident.$sys:ident.$fn:ident = $name:expr) => {
+        $metric.gauge($name, $sys.$fn() as f64);
+    };
+}
+
+macro_rules! gauge_network {
+    ($metric:ident.$sys:ident.$fn:ident = $name:expr, $x:expr) => {
+        $metric.gauge(
+            Key::from_parts($name, vec![Label::new("network", $x.to_owned())]),
+            $sys.$fn() as f64,
+        );
     };
 }
 
 impl Metric {
     /// Update gauge.
     pub fn gauge<K: Into<Key>>(&self, k: K, val: f64) {
+        if !self.enabled {
+            return;
+        }
         self.recorder
             .update_gauge(&k.into(), GaugeValue::Absolute(val));
     }
 
     /// Increment count.
     pub fn count_inc<K: Into<Key>>(&self, k: K, val: u64) {
+        if !self.enabled {
+            return;
+        }
         self.recorder.increment_counter(&k.into(), val);
     }
 
@@ -91,11 +109,17 @@ impl Metric {
         &self,
         listen: impl Fn(&Self) -> Result<(), PropertyError> + Send + 'static,
     ) {
+        if !self.enabled {
+            return;
+        }
         let mut guard = self.code.lock();
         guard.push(Box::new(listen));
     }
 
     fn flush(&self) -> Result<(), PropertyError> {
+        if !self.enabled {
+            return Ok(());
+        }
         let guard = self.code.lock();
         for i in guard.iter() {
             (i)(self)?;
@@ -110,12 +134,12 @@ impl Metric {
     }
 
     fn register_sysinfo(&self) {
+        let sys = self.sys.lock();
         let mut labels = vec![];
-        set_val!(labels:self.get_name => "name");
-        set_val!(labels:self.get_host_name => "hostname");
-        set_val!(labels:self.get_kernel_version => "kernel_version");
-        set_val!(labels:self.get_os_version => "os_version");
-        set_val!(labels:self.get_long_os_version => "long_os_version");
+        set_val!(labels:sys.get_name => "name");
+        set_val!(labels:sys.get_host_name => "hostname");
+        set_val!(labels:sys.get_kernel_version => "kernel_version");
+        set_val!(labels:sys.get_os_version => "os_version");
         let key = Key::from_parts("uptime", labels);
         self.gauge(
             key,
@@ -124,16 +148,73 @@ impl Metric {
                 .expect("Not possible")
                 .as_millis() as f64,
         );
-        gauge_kb!(self.get_total_memory = "system.memory_total");
-        gauge_kb!(self.get_total_swap = "system.memory_swap_total");
-        self.add_listen_state(|metric| {
+        gauge_kb!(self.sys.get_total_memory = "system.memory_total");
+        gauge_kb!(self.sys.get_total_swap = "system.swap_total");
+        let pid = get_current_pid().unwrap();
+        self.add_listen_state(move |metric| {
+            let mut sys = metric.sys.lock();
+            sys.refresh_memory();
             // Memory
-            gauge_kb!(metric.get_used_memory = "system.memory_used");
-            gauge_kb!(metric.get_free_memory = "system.memory_free");
-            gauge_kb!(metric.get_available_memory = "system.memory_available");
-            gauge_kb!(metric.get_used_swap = "system.memory_swap_used");
-            gauge_kb!(metric.get_free_swap = "system.memory_swap_free");
-            // Component
+            gauge_kb!(metric.sys.get_used_memory = "system.memory_used");
+            gauge_kb!(metric.sys.get_free_memory = "system.memory_free");
+            gauge_kb!(metric.sys.get_available_memory = "system.memory_available");
+            gauge_kb!(metric.sys.get_used_swap = "system.swap_used");
+            gauge_kb!(metric.sys.get_free_swap = "system.swap_free");
+            // Process
+            sys.refresh_process(pid);
+            let process = sys.get_processes().get(&pid).unwrap();
+            gauge_kb!(metric.process.memory = "process.memory");
+            gauge_kb!(metric.process.virtual_memory = "process.memory_virtual");
+            gauge!(metric.process.start_time = "process.uptime");
+            gauge!(metric.process.cpu_usage = "process.cpu_usage");
+            let disk = process.disk_usage();
+            metric.gauge(
+                "process.disk.total_written_bytes",
+                disk.total_written_bytes as f64,
+            );
+            metric.gauge("process.disk.written_bytes", disk.written_bytes as f64);
+            metric.gauge(
+                "process.disk.total_read_bytes",
+                disk.total_read_bytes as f64,
+            );
+            metric.gauge("process.disk.read_bytes", disk.read_bytes as f64);
+            // Network
+            sys.refresh_networks();
+            for (name, nt) in sys.get_networks() {
+                if !metric.networks.is_empty() && !metric.networks.contains(name) {
+                    continue;
+                }
+                gauge_network!(
+                    metric.nt.get_total_packets_received = "network.received.packets",
+                    name
+                );
+                gauge_network!(
+                    metric.nt.get_total_errors_on_received = "network.received.errors",
+                    name
+                );
+                gauge_network!(
+                    metric.nt.get_total_received = "network.received.total",
+                    name
+                );
+
+                gauge_network!(
+                    metric.nt.get_total_packets_transmitted = "network.transmitted.packets",
+                    name
+                );
+                gauge_network!(
+                    metric.nt.get_total_transmitted = "network.transmitted.total",
+                    name
+                );
+                gauge_network!(
+                    metric.nt.get_total_errors_on_transmitted = "network.transmitted.errors",
+                    name
+                );
+            }
+            sys.refresh_system();
+            let load = sys.get_load_average();
+            metric.gauge("system.load1", load.one);
+            metric.gauge("system.load5", load.five);
+            metric.gauge("system.load15", load.fifteen);
             Ok(())
         });
     }
@@ -149,6 +230,10 @@ pub struct MetricConfig {
     version: String,
     #[salak(desc = "Metric address, default is :9000")]
     address: Option<SocketAddr>,
+    #[salak(desc = "Network metrics")]
+    networks: HashSet<String>,
+    #[salak(default = "true")]
+    enabled: bool,
 }
 
 macro_rules! set_config {
@@ -180,7 +265,9 @@ impl Resource for Metric {
             recorder,
             code: Mutex::new(Vec::new()),
             handle,
-            sys: System::new_all(),
+            sys: Mutex::new(System::new_all()),
+            enabled: config.enabled,
+            networks: config.networks,
         };
         x.register_sysinfo();
         Ok(x)
